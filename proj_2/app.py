1d77721ec0efd2eb24b0530c12e3cf9afc403689
niharika3715 <94950412+niharika3715@users.noreply.github.com>
2023-03-05 23:39:20 +0530

    Update app.py

--- proj_2/app.py
+++ proj_2/app.py
@@ -16,7 +16,7 @@ if btn_click == True:
                  "the like minded people , Under the guidence of INNOMATICS team . "
                  " I Have been experiencing the life of a data science student with hands on projects. "
                  "This is being first interesting productive work in data visualization and responsive data app development. "
-                 "I am so much gratefull for this opputunity and particularly the teaching of KANAV BANSAL sir .""\n"
+                 "I am so much gratefull for this oppurtunity and particularly the teaching of KANAV BANSAL sir .""\n"
                  "Will get back to you with exciting works, See u After a little while ...")
     if col2.button('Connect with me on LinkedIn'):
         webbrowser.open_new_tab(url)
